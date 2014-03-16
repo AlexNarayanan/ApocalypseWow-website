96b65455a2e454871a85d82c2371818018e9015c
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-16 14:50:58 -0400

    Add Music
    Music page

---
+++ music.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>ApocalypseWow</title>
+        <link rel="icon" type="image/png" href="images/favicon.png"/>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <!-- navbar -->
+        <?php require 'common/navbar.php' ?>    
+        <div class="banner">
+            <div class="background" id="music-banner">
+                <img src="/images/music-banner.jpg" alt="banner"/>
+            </div>
+
+        <div id="wrapper">
+            <h3>End of the World Hits</h3>
+            <p>Not all songs about armageddon have to be gloomy and foreboding.
+                Sometimes it's about looking for the brightest spot in the pitch black abyss of the apocalypse.
+                Check out this top hits playlist including appearances from popular artists such as
+                Dragons, Janelle Monae, and Linkin Park!</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/videoseries?list=PLLaCMFBzwOGAu2lb2MAx6zxy9p6VvhkRD" 
+                    frameborder="0" allowfullscreen></iframe>
+            <h3>High Octane Apocalypse</h3>
+            <p>This playlist is perfect for those who are willing to face the end of the 
+                world head on and without fear. The aggressive tones and dark lyrics 
+                remind Features musical compositions by Tool, Rage Against the Machine, Faith No More, and others</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/videoseries?list=PLLaCMFBzwOGBUTDz5jAQIFOAYrWh-zj_k" 
+                    frameborder="0" allowfullscreen></iframe>
+            <h3>VGM/Instrumental Apocalyptic Mix</h3>
+            <p>Just because a song doesn't have words doesn't mean it can't be
+                about the apocalypse! Varying tones can create a dramatic effect,
+                implying a dire or out-of-this-world situation. 
+                Beats by Daft Punk, Chrono Trigger, The Prodigy, and more!</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/videoseries?list=PLLaCMFBzwOGDdjtGyqF5W3sh64obbE3Pv" 
+                    frameborder="0" allowfullscreen></iframe>
+        </div>
+     
+        <!-- Footer -->
+        <?php require 'common/footer.php'; ?>
+    </body>
+</html>
