db0daeae3628e5d4ef0b764841e0e20d70927031
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-16 12:05:35 -0400

    Common Page Elements

---
+++ common/footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+    <p>
+        Designed by Alex Narayanan and Jay Fok. 
+        Copyright © 2014, All Rights Reserved. 
+    </p>
+</div>
---
+++ common/navbar.php
@@ -0,0 +1,15 @@
+<div id="navbar">
+            <a href="index.php"><img src="/images/logo.png"/></a>
+    <ul>
+        <span class="divider"></span>
+        <a href="index.php"><li>Home</li></a>
+        <span class="divider"></span>
+        <a href="music.php"><li>Music</li></a>
+        <span class="divider"></span>
+        <a href=""><li>Resources</li></a>
+        <span class="divider"></span>
+        <a href=""><li>EBoard</li></a> 
+        <span class="divider"></span>
+        <a href="about.php"><li>About</li></a>
+    </ul>
+</div>
