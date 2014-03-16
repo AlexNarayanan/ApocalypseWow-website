5e4f4092b31d121c09c1b1b92de8b35b018863eb
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-16 11:56:58 -0400

    index.php
    added index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="keywords" content="Apocalypse, Armageddon, Wowe, Doge"/>
+        <meta name="description" content="A look into humanuty's obsession with the apocalypse"/>
+
+        <title>ApocalypseWow</title>
+        <link rel="icon" type="image/png" href="images/favicon.png"/>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <!-- navbar -->
+        <?php require 'common/navbar.php' ?>    
+        
+        <div class="banner">
+            <div class="background">
+                <img src="/images/banner.jpg" alt="banner"/>
+            </div>
+            <h2>Some say we'll see armageddon soon</h2>
+        </div>
+        
+        <div id="title">
+            <h1>A closer look at humanity's obsession with the apocalypse</h1>
+        </div>
+        
+        <div id="info-block">
+            <div id="left" class="block">
+                <img src="/images/music.png" alt="music"/>
+                <p>You may not have known that these familiar songs were about the apocalypse.</p>
+            </div>
+            <div id="middle" class="block">
+                <img src="/images/news.png" alt="writing"/>
+            </div>
+            <div id="right" class="block">
+                <img src="/images/tv.png" alt="video"/>
+            </div>
+        </div>
+        
+        <?php require 'common/footer.php'; ?>
+    </body>
+</html>
+
+
+
+
+
