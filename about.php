588c0786af014d2213f73ce19dd452fadcb82df3
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-16 14:50:24 -0400

    Add About
    About page

---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>     
+        <title>ApocalypseWow | About</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <link rel="icon" type="image/png" href="images/favicon.png" />
+        <link rel="stylesheet" href="stylesheet.css" type="text/css" charset="utf-8" />
+	<link href='http://fonts.googleapis.com/css?family=Corben:700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <?php require 'common/navbar.php'; ?>
+        <div class="banner">
+            <div class="background">
+                <img src="/images/about-banner.jpg" alt="banner"/>
+            </div>
+
+        </div>
+        <div id="wrapper">
+            <h1>ApocalypseWow is brought to you by two Computer Science majors
+            at Northeastern University</h1>
+            <div class="column">
+                <img class="headshot" src="images/alex.jpg" alt="alex"/>
+                <div class="info">
+                    <h2>Alex</h2>
+                    <h4>San Jose, CA</h4>
+                </div>
+            </div>
+            <div class="column">
+                <img class="headshot" src="images/jay.jpg" alt="jay"/>
+                <h2>Jay Fok</h2>
+                <h4>Boston, MA</h4>
+            </div>
+        </div>
+        
+        <?php require 'common/footer.php'; ?>
+    </body>
+</html>
