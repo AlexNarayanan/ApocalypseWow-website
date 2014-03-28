4ec3854d4b20ebe4c8fb9ccec27e52c355598c9b
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-28 00:46:43 -0400

    Add video

---
+++ video.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>ApocalypseWow | Video</title>
+        <link rel="icon" type="image/png" href="images/favicon.png"/>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <!-- navbar -->
+        <?php require 'common/navbar.php' ?>  
+        <div class="banner">
+            <div class="background" id="video-banner">
+                <img src="/images/banner5.jpg" alt="banner"/>
+            </div>
+        </div>
+        
+        <div id="wrapper">
+            <p>Many of the films we know and love are in fact riddles with apocalyptic themes.
+            Audiences worldwide are becoming more and more attracted to seeing the hero
+            escape and survive the end of the world, especially during the last decade. 
+            Here are some of our favorite apocalyptic films.</p>
+            <h2>Pacific Rim</h2>
+            <p>This godzilla-esque is the epitome of a classic apocalyptic movie.
+            The survival of the world is at stake, and humanity is forced to band 
+            together to defeat a terrifying and monstrous opponent.</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/5guMumPFBag" 
+                    frameborder="0" allowfullscreen></iframe>
+            <h2>I, Robot</h2>
+            <p>This movie puts a fresh spin on the robot apocalypse. Every time
+            you watch it you'll be certain to pick up something new and interesting
+            that you missed the previous time. I, Robot resembles everything a 
+            robot takeover movie should be and more.</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/rL6RRIOZyCM" 
+                    frameborder="0" allowfullscreen></iframe>
+            <h2>Dawn of the Dead</h2>
+            <p>This zombie apocalypse film draws many similarities with the video
+            game series Left 4 Dead. Human survivors of the infection take 
+            refuge in a mall, using any means at their disposal to try and 
+            outlast the apocalypse.</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/36X8C7nlbE4" 
+                    frameborder="0" allowfullscreen></iframe>
+            <h2>Wall-E</h2>
+            <p>Disney and Pixar never disappoint. Wall-E is a splendid example
+            of an apocalyptic themed film that caters to all ages. After looking past
+            the classic Disney humor and charm, viewers can't help but wonder if
+            something like this is truly in store for the earth in the distant future.</p>
+            <iframe width="640" height="360" 
+                    src="//www.youtube.com/embed/alIq_wG9FNk" 
+                    frameborder="0" allowfullscreen></iframe>
+        </div>     
+        <!-- Footer -->
+        <?php require 'common/footer.php'; ?>
+    </body>
+</html>
