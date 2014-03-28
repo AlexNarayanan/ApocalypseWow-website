29964d0f721c22b50d2ee81a4b76e5953c60e858
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-28 00:47:21 -0400

    Add games

---
+++ games.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>ApocalypseWow | Games</title>
+        <link rel="icon" type="image/png" href="images/favicon.png"/>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <!-- navbar -->
+        <?php require 'common/navbar.php' ?>    
+        <div class="banner">
+            <div class="background" id="games-banner">
+                <img src="/images/banner6.png" alt="banner"/>
+            </div>
+        </div>
+        
+        <div id="wrapper">
+            <h2>The Apocalypse in Gaming</h2>
+            <p></p>
+        </div>
+     
+        <!-- Footer -->
+        <?php require 'common/footer.php'; ?>
+    </body>
