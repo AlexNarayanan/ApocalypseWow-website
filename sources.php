67da7bbea63df65e3373de1bbb7f62563a476507
AlexNarayanan <narayanan.a@husky.neu.edu>
2014-03-28 00:45:59 -0400

    Add sources

---
+++ sources.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>ApocalypseWow</title>
+        <link rel="icon" type="image/png" href="images/favicon.png"/>
+        <link rel="stylesheet" href="stylesheet.css" type="text/css"/>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <!-- navbar -->
+        <?php require 'common/navbar.php' ?>    
+        <div class="banner">
+            <div class="background" id="music-banner">
+                <img src="/images/banner4.png" alt="banner"/>
+            </div>
+        </div>
+        
+        <div id="wrapper">
+            <h2>Films Referenced</h2>
+            <p>del Toro, Guillermo dir. Pacific Rim. Warner Bros, 2013. Film.</p>
+            <p>Proyas, Alex, dir. I, Robot. Twentieth Century Fox, 2004. Film.</p>
+            <p>Snyder, Zach, dir. Dawn of the Dead. Universal Pictures, 2004. Film.</p>
+            <p>Standon, Andrew, dir. Wall-E. Disney Pixar, 2008. Film.</p>
+        </div>
+     
+        <!-- Footer -->
+        <?php require 'common/footer.php'; ?>
+    </body>
+</html>
+
